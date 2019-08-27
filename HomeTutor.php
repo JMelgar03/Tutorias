c5e52b9d9dd04cda236bc88e3cd186dec9c82391
Hector504 <bardaleshector41@gmail.com>
2019-08-27 15:33:52 -0500

    actualizaciones

--- HomeTutor.php
+++ HomeTutor.php
@@ -22,7 +22,7 @@
       <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
       <link rel="stylesheet" type="text/css" href="css/util.css">
       <link rel="stylesheet" type="text/css" href="css/main.css">
-      <link rel="stylesheet" type="text/css" href="css/HomeEstudiante.css">
+      <link rel="stylesheet" type="text/css" href="css/homeTutor.css">
 
       <!-- Bootstrap core CSS -->
       <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -35,7 +35,7 @@
   
       <body>
         <!-- Navigation -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top" id="navbar1">
       <div class="container">
 
         <a class="navbar-brand" href="index.php"><img src="img/imgunah/logo.png">UNAH</a>
@@ -63,7 +63,7 @@
                 
         <!--        <a class="dropdown-item" href="portfolio-3-col.html"></a>
                 <a class="dropdown-item" href="portfolio-4-col.html">4 Column Portfolio</a>
-                <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a> -->
+                <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a>-->
               </div>
            <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -78,7 +78,85 @@
         </div>
       </div>
     </nav>
+ <!-- Navigation -->
+ <nav class="navbar fixed-top navbar-expand-lg   fixed-top" id="navbar1">
+    <div class="container">
 
+      <a class="navbar-brand" href="HomeEstudiante.php"><img src="img/imgunah/logo.png">UNAH</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+
+
+    
+
+
+      </div>
+
+
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-arrow-circle-down fa-2x"></i><br> Descargar
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+          <a class="dropdown-item" href="manualUsuario.pdf"><i class="fas fa-book"></i> Manual de usuario</a>
+        </div>
+      </li>
+
+      <!--empiezoo-->
+      <li class="nav-item">
+            <a class="nav-link" href="solicitudes.php"><i class="fas fa-bell fa-2x"></i><br>Solicitudes</a>
+      </li>
+
+        
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-user fa-2x"></i><br><?php
+         echo '<label>'  . $_SESSION["email"] . '</label>'
+           ?>
+
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+        <a class="dropdown-item" href="Perfil.php"><i class="fas fa-user"></i> Mi Perfil</a> 
+         <a class="dropdown-item" href="index.php"><i class="fas fa-user-times"></i> Cerar Sesion</a>
+
+          <!--        <a class="dropdown-item" href="portfolio-3-col.html"></a>
+                <a class="dropdown-item" href="portfolio-4-col.html">4 Column Portfolio</a>
+                <a class="dropdown-item" href="portfolio-item.html">Single Portfolio Item</a> -->
+        </div>
+      </li>
+
+
+      <!-- </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Blog
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
+                <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
+                <a class="dropdown-item" href="blog-post.html">Blog Post</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Other Pages
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                <a class="dropdown-item" href="full-width.html">Full Width Page</a>
+                <a class="dropdown-item" href="sidebar.html">Sidebar Page</a>
+                <a class="dropdown-item" href="faq.html">FAQ</a>
+                <a class="dropdown-item" href="404.html">404</a>
+                <a class="dropdown-item" href="pricing.html">Pricing Table</a> -->
+    </div>
+    </li>
+    </ul>
+    </div>
+    </div>
+  </nav>
 
     <!-- ....................................INICIO MODAL................................................-->
 
